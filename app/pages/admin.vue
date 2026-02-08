2fadb92cba9de4432d58e79b9d0510395bc4a860
Roya <a78945612385238@gmail.com>
2026-02-08 20:02:50 +0800

    feat: init

---
+++ app/pages/admin.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { data } = useAsyncData(
+  "users",
+  () => {
+    return $fetch("https://jsonplaceholder.typicode.com/users");
+  },
+  {
+    immediate: true,
+    lazy: true,
+  },
+);
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
+  routeRules: {
+    "/admin": {
+      ssr: false,
+    },
+  },
+});
